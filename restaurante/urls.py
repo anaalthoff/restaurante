07bbc3c27185b9046f5c533d9d1a1097ec2bff1b
Ana Paula Althoff <ana.althoff@srssistemas.com>
2025-05-29 22:29:58 -0300

    feat: corrige rota 'historia' para usar a view correta e adiciona template historia.html

--- restaurante/urls.py
+++ restaurante/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = 'index'),
-    path('historia', views.index, name = 'historia'),
+    path('historia', views.historia, name = 'historia'),
 ]
